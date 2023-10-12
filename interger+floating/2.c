f4daa40dceb23ce65dd684f47a313022202ff504
Star <i@xingbest.xyz>
2023-10-12 20:40:06 +0800

    Your commit message

--- interger+floating/2.c
+++ interger+floating/2.c
@@ -1,3 +1,42 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) { printf("yes"); }
+int test();
+
+int main() {
+  bool success = false;
+
+  int result = test();
+  if (result == 0) {
+    success = true;
+  }
+
+  if (success) {
+    printf("You are successful\n");
+  } else {
+    printf("Sorry, you didn't succeed\n");
+  }
+
+  return 0;
+}
+
+int test() {
+  char password[1024];
+  int attempts = 3;
+  while (attempts > 0) {
+    printf("Enter your password (%d attempts left):\t", attempts);
+    scanf("%s", password);
+
+    if (strcmp(password, "8726") == 0) {
+      return 0;
+    } else {
+      printf("Password incorrect. Try again.\n");
+      attempts--;
+    }
+  }
+
+  return 1;
+}
+
